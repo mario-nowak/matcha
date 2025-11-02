919c8427a6584cfca40f1f36a1b28cc368d7a822
Mario Nowak <48293574+mario-nowak@users.noreply.github.com>
2025-11-02 15:00:16 +0100

    Add very first basic lexer

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const lexer = @import("lexer.zig");
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -12,6 +14,14 @@ pub fn main() !void {
     const cwd = std.fs.cwd();
     const fileContents = try cwd.readFileAlloc(allocator, fileName, 4096);
     defer allocator.free(fileContents);
+
+    var lexerTest = lexer.Lexer.init(fileContents, allocator);
+    defer lexerTest.deinit();
+
+    const token = lexerTest.next();
+
+    // Print token directly
+    std.debug.print("Token: {f}\n", .{token});
 
     // Print file contents
     std.debug.print("{s}", .{fileContents});
