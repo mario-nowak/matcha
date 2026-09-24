1fffe642bdc9cd3b4064b22844ebebe9d0dc7001
Mario Nowak <m.now@protonmail.com>
2026-09-24 18:22:12 +0200

    Remove type checker wrapper

--- matcha-compiler/src/compiler/pipeline.zig
+++ matcha-compiler/src/compiler/pipeline.zig
@@ -26,9 +26,6 @@ pub fn generateLlvmIrFromFile(
 
     const name_resolver = semantic_analysis.name_resolution.NameResolver.init(allocator, diagnostic_store);
     const node_type_analyzer = semantic_analysis.type_checking.NodeTypeAnalyzer.init(allocator, diagnostic_store);
-    const type_checker = semantic_analysis.type_checking.TypeChecker.init(
-        node_type_analyzer,
-    );
     const structural_validator = semantic_analysis.control_flow_validation.StructuralValidator.init(diagnostic_store);
     const exit_behavior_analyzer = semantic_analysis.control_flow_validation.ExitBehaviorAnalyzer.init(
         allocator,
@@ -43,7 +40,7 @@ pub fn generateLlvmIrFromFile(
     );
     var semantic_analyzer = semantic_analysis.SemanticAnalyzer.init(
         name_resolver,
-        type_checker,
+        node_type_analyzer,
         control_flow_validator,
         runtime_representation_analyzer,
     );
